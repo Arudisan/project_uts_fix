8750208afc2a54d6219459319801060ce8db7df9
Ardi <ardibbmme@gmail.com>
2022-10-29 08:49:44 +0700

    changes

---
+++ database/Models/CategoryProduct.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CategoryProduct extends Model
+{
+    use HasFactory;
+
+    protected $fillable=['name','description','status'];
+
+    public function product(){
+        return $this->hasMany(product::class, 'category_id', 'id');
+    }
+}
